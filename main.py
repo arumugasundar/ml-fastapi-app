624ad3a352c0d5545181d8dbbd3c318c4da73dc6
arumugasundar <arumugasundar29@gmail.com>
2024-10-18 16:43:15 +0530

    feat : model setup added

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-from typing import Optional
+import pickle
+import numpy as np
+import PIL.Image
+import PIL.ImageOps
 
-from fastapi import FastAPI
+with open('mnist_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict-image/")
+async def predict_image(UploadFile = File(...)):
+    contents = await file.read()
+    pil_image = Image.open(io.BytesIO((contents)).convert("L"))
+    pil_image = PIL.ImageOps.invert(pil_image)
+    pil_image = pil_image.resize((28,28), PIL.Image.ANTIALIAS)
+    img_array = np.array(pil_image).reshape(1,-1)
+    prediction = model.predict(img_array)
+    return {"prediction" : int(prediction[0])}
 
 
 @app.get("/")
